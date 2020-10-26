1fbd527c6cc72212680b522aa76a89d1842487fc
Julian_Ehiem <jules.ehiem@gmail.com>
2020-10-26 19:22:49 -0400

    Hid api key for security

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const URL = "https:/api.openweathermap.org/data/2.5/weather";
-const API_KEY = "92a368be320fc5e9a0816ae44a1a686f";
+// const API_KEY = "92a368be320fc5e9a0816ae44a1a686f";
 
 export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
     params: {
       q: query,
       units: "metric",
-      APPID: API_KEY,
+      APPID: process.env.REACT_APP_SECRET_API_KEY,
     },
   });
 
