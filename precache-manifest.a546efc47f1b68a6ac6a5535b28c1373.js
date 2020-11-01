ab3c32067f3c0c4135594318e74ec8a1ae44100e
Julian_Ehiem <jules.ehiem@gmail.com>
2020-11-01 09:54:54 -0500

    Updates

---
+++ precache-manifest.a546efc47f1b68a6ac6a5535b28c1373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6a54cb8c4154ae7417c13b79b18fc9",
+    "url": "/weatherhub/index.html"
+  },
+  {
+    "revision": "aba12c539c3073954d2a",
+    "url": "/weatherhub/static/css/main.400dbc18.chunk.css"
+  },
+  {
+    "revision": "ad0dadc60650d657544e",
+    "url": "/weatherhub/static/js/2.0878dcf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherhub/static/js/2.0878dcf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba12c539c3073954d2a",
+    "url": "/weatherhub/static/js/main.f05dbc3e.chunk.js"
+  },
+  {
+    "revision": "a6f3b31d710f67b50f01",
+    "url": "/weatherhub/static/js/runtime-main.a5b7bff7.js"
+  },
+  {
+    "revision": "aaaf641310dd9c2ef2174845a4165e39",
+    "url": "/weatherhub/static/media/bg-img.aaaf6413.jpg"
+  }
+]);
