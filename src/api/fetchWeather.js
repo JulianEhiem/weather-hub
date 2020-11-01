5a50f098eaaddd1db927465aaa3e41c8e39c95dc
Julian_Ehiem <jules.ehiem@gmail.com>
2020-11-01 10:03:21 -0500

    api cleanup

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,21 +1,3 @@
-// import axios from "axios";
-
-// const URL = "https:/api.openweathermap.org/data/2.5/weather";
-// // const API_KEY = "92a368be320fc5e9a0816ae44a1a686f";
-// const REACT_APP_SECRET_API_KEY = "92a368be320fc5e9a0816ae44a1a686f";
-
-// export const fetchWeather = async (query) => {
-//   const { data } = await axios.get(URL, {
-//     params: {
-//       q: query,
-//       units: "metric",
-//       // APPID: process.env.REACT_APP_SECRET_API_KEY,
-//       APPID: REACT_APP_SECRET_API_KEY,
-//     },
-//   });
-//   return data;
-// };
-
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
